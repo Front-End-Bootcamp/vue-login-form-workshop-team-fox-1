aae3c6762ecdbd0153d02beab6b6f894acc9a6e7
mfurkanakin <mfakin@yahoo.com>
2022-10-02 12:08:07 +0300

    password comp building

---
+++ src/components/Input/password.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps(['password']);
+const emit = defineEmits(['password']);
+
+const passwordHandler = event => {
+	console.log('passwordHandler' , /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+	.test(event.target.value))
+}
+
+</script>
+
+
+<template>
+	<form>
+		<label ></label>
+		<input class="from__input" type="password" name="password" id="password"
+		placeholder="Password" :value="password" @input="passwordHandler">
+	</form>
+</template>
